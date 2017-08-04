5e302f32363ea8186db9b88e3c1990fa19463dc8
lincoln <huylincoln007@yahoo.com>
2017-08-04 15:47:32 -0700

    show list of doctors, address, phone numbers by appending

--- js/doctor.js
+++ js/doctor.js
@@ -7,12 +7,10 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
-      // var arrayDoctors = [];
-      for (i = 0; i < result.data[0].practices.length; i++) {
-        var example = result.data[0].practices[i].name;
-        console.log(example)
-        // arrayDoctors.push(example)
-        $('.showDoctor').append("<h4>" + result.data[i].practices[0].name + "</h2>" + result.data[i].practices[0].visit_address.street + "<br>" + result.data[i].practices[0].visit_address.city + ", " + result.data[i].practices[0].visit_address.state + " " + result.data[i].practices[0].visit_address.zip);
+      for (i = 0; i < result.data.length; i++) {
+        var response = result.data[i].practices[0].name;
+        console.log(response);
+        $('.showDoctor').append("<h4>" + result.data[i].practices[0].name + "</h4>" + result.data[i].practices[0].visit_address.street + "<br>" + result.data[i].practices[0].visit_address.city + ", " + result.data[i].practices[0].visit_address.state + " " + result.data[i].practices[0].visit_address.zip + "<br>" + "Phone Number: " + result.data[i].practices[0].phones[0].number);
       }
     })
    .fail(function(error){
