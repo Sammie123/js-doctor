d7a4f20e2b92bc9cd23670ab4875636f120e851a
lincoln <huylincoln007@yahoo.com>
2017-08-04 16:08:42 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# BetterDoctor App
+
+#### An app that is finds doctors based on your disease/symptoms, Auguest 4, 2017
+
+#### By Lincoln Nguyen
+
+## Description
+Doctor app that finds doctors that specialized based on your symptoms or disease. User can input 'cancer' and results will display twenty doctors/clinics with address and phone number.
+
+## Setup/Installation Requirements
+* Clone this respository onto local drive
+* In terminal,
+  * type 'npm install'
+  * type 'bower install'
+* Once installed, in the terminal..
+  * type 'gulp build'
+  * type 'gulp serve'
+
+## Known Bugs
+No known bugs at this time
+
+## Support and contact details
+For questions or feedback Lincoln Nguyen at huylincoln007@yahoo.com_
+
+## Technologies Used
+* JavaScript
+  * Node.js
+* JQuery
+* HTML
+
+### License
+*This software runs under the MIT license*
+
+Copyright (c) 2017 **_MIT License_**
--- js/doctor.js
+++ js/doctor.js
@@ -7,11 +7,14 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
+      var arrayList = [];
       for (i = 0; i < result.data.length; i++) {
         var response = result.data[i].practices[0].name;
+        arrayList.push(response);
         console.log(response);
         $('.showDoctor').append("<h4>" + result.data[i].practices[0].name + "</h4>" + result.data[i].practices[0].visit_address.street + "<br>" + result.data[i].practices[0].visit_address.city + ", " + result.data[i].practices[0].visit_address.state + " " + result.data[i].practices[0].visit_address.zip + "<br>" + "Phone Number: " + result.data[i].practices[0].phones[0].number);
       }
+      // $('.showDoctor').append("<li>" + arrayList + "</li>");
     })
    .fail(function(error){
       console.log("fail");
